4f47b8fae33ec671172f74c3484e7e59a35e489d
Iwo Plaza <iwoplaza@gmail.com>
2024-09-28 22:45:31 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,25 +4,33 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  base: '/velosentry-docs',
+  integrations: [
+    starlight({
+      title: 'VeloSentry',
+      logo: {
+        light: '/public/horizontal-logo-light.svg',
+        dark: '/public/horizontal-logo-dark.svg',
+        alt: 'VeloSentry Logo',
+        replacesTitle: true,
+      },
+      social: {
+        github:
+          'https://github.com/Software-Dimension-Hackyeah-2024/velosentry',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', slug: 'guides/example' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
